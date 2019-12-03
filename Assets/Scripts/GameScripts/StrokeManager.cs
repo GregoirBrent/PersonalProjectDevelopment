d13dbea38ade727c3eb340a21ece9dca7acbcddb
GregoirBrent <brentgregoir@hotmail.com>
2019-12-03 15:56:23 +0100

    Added functionality to player

--- Assets/Scripts/GameScripts/StrokeManager.cs
+++ Assets/Scripts/GameScripts/StrokeManager.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using System.IO.Ports;
 
 public class StrokeManager : MonoBehaviour
 {
 
-    void start()
+    void Start()
 	{
-		FindPlayerBall();
-	}
+        //sp.Open();
+        //sp.ReadTimeout = 1;
+
+        FindPlayerBall();
+        StrokeCount = 0;
+    }
+
+    //SerialPort sp = new SerialPort("/dev/cu.usbmodem14201", 9600);
+    //int Arduino = 0;
 
     public float StrokeAngle { get; protected set; }
+    public float StrokeForce { get; protected set; }
+    public int StrokeCount { get; protected set; }
+
+    float strokeForceFillSpeed = 5f;
+    int fillDir = 1;
+    float MaxStrokeForce = 10f;
+
+    public enum StrokeModeEnum { AIMING, FILLING, DO_HIT, BALL_IS_ROLLING };
+    public StrokeModeEnum StrokeMode { get; protected set; }
+
+    public float StrokeForcePerc { get { return StrokeForce / MaxStrokeForce; } }
 
     Rigidbody playerBallRB;
 
@@ -34,7 +53,83 @@ public class StrokeManager : MonoBehaviour
 
     private void Update() //Gebruiken voor visuele frames/inputs
     {
-        StrokeAngle += Input.GetAxis("Horizontal") * 100f * Time.deltaTime; 
+        //if (sp.IsOpen)
+        //{
+        //    try
+        //    {
+        //        int Value = sp.ReadByte();
+        //        Arduino = Value;
+        //    }
+        //    catch (System.Exception)
+        //    {
+        //        return;
+        //    }
+
+        //}
+
+        //Debug.Log(++Counter + " Arduino: " + Arduino);
+
+        if (StrokeMode == StrokeModeEnum.AIMING)
+        {
+
+            StrokeAngle += Input.GetAxis("Horizontal") * 100f * Time.deltaTime;
+
+            //if (Arduino == 2)
+            //{
+            //    //Debug.Log("joystick +1");
+            //    StrokeAngle += 1f;
+            //}
+
+            //if (Arduino == 3)
+            //{
+            //    //Debug.Log("joystick -1");
+            //    StrokeAngle -= 1f;
+            //}
+
+            if  (Input.GetButtonUp("Fire1") /*Arduino == 1*/)
+            {
+                //Debug.Log("volume");
+                StrokeMode = StrokeModeEnum.FILLING;
+                return;
+            }
+
+        }
+
+
+
+        if (StrokeMode == StrokeModeEnum.FILLING)
+        {
+            StrokeForce += (strokeForceFillSpeed * fillDir) * Time.deltaTime;
+
+            if (StrokeForce > MaxStrokeForce)
+            {
+                StrokeForce = MaxStrokeForce;
+                fillDir = -1;
+            }
+            else if (StrokeForce < 0)
+            {
+                StrokeForce = 0;
+                fillDir = 1;
+            }
+
+            if (Input.GetButtonUp("Fire1"))
+            {
+                //GameObject.FindGameObjectWithTag("arrow").SetActive(false);
+                StrokeMode = StrokeModeEnum.DO_HIT;
+            }
+        }
+    }
+
+
+    void CheckRollingStatus()
+    {
+        //is de bal nog aan het rollen?
+        if (playerBallRB.IsSleeping())
+        {
+            //StrokeCount++;
+            //GameObject.FindGameObjectWithTag("arrow").SetActive(false);
+            StrokeMode = StrokeModeEnum.AIMING;
+        }
     }
 
 
@@ -46,9 +141,25 @@ public class StrokeManager : MonoBehaviour
             return;
         }
 
-        Vector3 forceVec = new Vector3(0, 0, 5f);
+        if (StrokeMode == StrokeModeEnum.BALL_IS_ROLLING)
+        {
+            CheckRollingStatus();
+            return;
+        }
+
+        if (StrokeMode != StrokeModeEnum.DO_HIT)
+        {
+            return;
+        }
+
+
+        Vector3 forceVec = new Vector3(0, 0, StrokeForce);
 
         playerBallRB.AddForce(Quaternion.Euler(0, StrokeAngle, 0) * forceVec, ForceMode.Impulse);
-        
+
+        StrokeForce = 0;
+        fillDir = 1;
+        StrokeCount++;
+        StrokeMode = StrokeModeEnum.BALL_IS_ROLLING;
     }
 }
---
+++ Assets/Scripts/GameScripts/UncapPhysicsSpeeds.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UncapPhysicsSpeeds : MonoBehaviour
+{
+
+   
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        if (rb == null)
+        {
+            Debug.LogError("No RidgidBody found on" + gameObject.name);
+            return;
+        }
+
+        rb.maxAngularVelocity = Mathf.Infinity;
+
+        Destroy(this);
+    }
+
+   
+}
