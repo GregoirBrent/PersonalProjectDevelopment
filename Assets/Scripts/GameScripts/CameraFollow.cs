cf0e2b2fd69532444ce944f2811e057e875e13a3
GregoirBrent <brentgregoir@hotmail.com>
2019-12-04 17:30:01 +0100

    Added  rotate camera around player

--- Assets/Scripts/GameScripts/CameraFollow.cs
+++ Assets/Scripts/GameScripts/CameraFollow.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-	public GameObject Ball;
+	public Transform Ball;
+
+    public float turnSpeed = 4.0f;
 
 	private Vector3 CamereOffset;
 
     void Start()
     {
-        CamereOffset = transform.position - Ball.transform.position; 
+        //CamereOffset = transform.position - Ball.transform.position;
+
+        CamereOffset = new Vector3(Ball.position.x, Ball.position.y + 8.0f, Ball.position.z + 7.0f);
     }
 
     void LateUpdate()
     {
-        transform.position = Ball.transform.position + CamereOffset; 
+        //transform.position = Ball.transform.position + CamereOffset;
+
+        CamereOffset = Quaternion.AngleAxis(Input.GetAxis("RoteerX") * turnSpeed, Vector3.up) * CamereOffset;
+        CamereOffset = Quaternion.AngleAxis(Input.GetAxis("RoteerY") * turnSpeed, Vector3.up) * CamereOffset;
+        transform.position = Ball.transform.position + CamereOffset;
+        transform.LookAt(Ball.position);
+
     }
 
 }
