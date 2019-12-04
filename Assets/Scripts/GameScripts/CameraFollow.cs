9a4d61c3bd6238516176273aa8af8cd82cf8ee7d
GregoirBrent <brentgregoir@hotmail.com>
2019-12-04 20:52:57 +0100

    camera settings fixt

--- Assets/Scripts/GameScripts/CameraFollow.cs
+++ Assets/Scripts/GameScripts/CameraFollow.cs
@@ -7,25 +7,55 @@ public class CameraFollow : MonoBehaviour
 	public Transform Ball;
 
     public float turnSpeed = 4.0f;
+   
 
-	private Vector3 CamereOffset;
+	private Vector3 CamereOffsetX;
+    private Vector3 CamereOffsetY;
+
+    public float height = 1f;
+    public float distance = 2f;
 
     void Start()
     {
-        //CamereOffset = transform.position - Ball.transform.position;
 
-        CamereOffset = new Vector3(Ball.position.x, Ball.position.y + 8.0f, Ball.position.z + 7.0f);
+
+        CamereOffsetX = new Vector3(0, height, distance);
+        CamereOffsetY = new Vector3(0, 0, distance);
     }
 
     void LateUpdate()
     {
-        //transform.position = Ball.transform.position + CamereOffset;
 
-        CamereOffset = Quaternion.AngleAxis(Input.GetAxis("RoteerX") * turnSpeed, Vector3.up) * CamereOffset;
-        CamereOffset = Quaternion.AngleAxis(Input.GetAxis("RoteerY") * turnSpeed, Vector3.up) * CamereOffset;
-        transform.position = Ball.transform.position + CamereOffset;
+        CamereOffsetX = Quaternion.AngleAxis(Input.GetAxis("RoteerX") * turnSpeed, Vector3.up) * CamereOffsetX;
+
+        CamereOffsetY = Quaternion.AngleAxis(Input.GetAxis("RoteerY") * turnSpeed, Vector3.right) * CamereOffsetY;
+
+
+
+        if (Ball.rotation.eulerAngles.x > 45f && Ball.rotation.eulerAngles.x < 180f)
+        {
+            Ball.rotation = Quaternion.Euler(45f, 0, 0);
+        }
+
+        if (Ball.rotation.eulerAngles.x > 180f && Ball.rotation.eulerAngles.x < 300f)
+        {
+            Ball.rotation = Quaternion.Euler(300f, 0, 0);
+        }
+
+        float desiredXAngle = Ball.eulerAngles.x;
+        float desiredYAngle = Ball.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        //transform.position = Ball.position - (rotation * CamereOffsetX);
+
+        transform.position = Ball.transform.position + CamereOffsetX + CamereOffsetY;
+
+        if (transform.position.y < Ball.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, Ball.position.y, transform.position.z);
+        }
+
         transform.LookAt(Ball.position);
 
-    }
+       }
 
-}
+    }
