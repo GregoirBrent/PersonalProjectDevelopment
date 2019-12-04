4af9047e604c591f80f64624e542702e5da07067
GregoirBrent <brentgregoir@hotmail.com>
2019-12-04 17:02:02 +0100

    added cammera follow to player

--- Assets/Scripts/GameScripts/CameraFollow.cs
+++ Assets/Scripts/GameScripts/CameraFollow.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-	public Transform PlayerTransform;
+	public GameObject Ball;
+
 	private Vector3 CamereOffset;
 
-    [Range(0.01f, 1.0f)]
-	public float SmoothFactor = 0.5f;
-
     void Start()
-	{
-        CamereOffset = transform.position - PlayerTransform.position;
-	}
+    {
+        CamereOffset = transform.position - Ball.transform.position; 
+    }
 
     void LateUpdate()
-	{
-		Vector3 newPos = PlayerTransform.position + CamereOffset;
-		PlayerTransform.position = Vector3.Slerp(PlayerTransform.position, newPos, SmoothFactor);
-	}
+    {
+        transform.position = Ball.transform.position + CamereOffset; 
+    }
+
 }
