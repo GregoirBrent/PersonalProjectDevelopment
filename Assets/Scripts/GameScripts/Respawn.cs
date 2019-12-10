861330661ac07d958a34d3a7db86fb192ab0cd83
GregoirBrent <brentgregoir@hotmail.com>
2019-12-10 17:28:15 +0100

    AR respawn ball + stokecount works

--- Assets/Scripts/GameScripts/Respawn.cs
+++ Assets/Scripts/GameScripts/Respawn.cs
@@ -8,28 +8,28 @@ public class Respawn : MonoBehaviour
     [SerializeField]private Transform Ball;
 
     SpawnPoint SpawnPoint;
-    //float yOffset;
+    float yOffset = -5;
 
-    // void Start()
-    //{
-        
-    //    yOffset = Ball.transform.position.y - 50;
-    //}
+    void Start()
+    {
 
-    //void Update()
-    //{
-    ////   if(yOffset < Ball.position.y)
-    ////    {
-    ////        Rigidbody BallRB = Ball.GetComponent<Rigidbody>();
+    }
 
-    ////        //zet angularVelocity en velocity to 0
-    ////        BallRB.angularVelocity = Vector3.zero;
-    ////        BallRB.velocity = Vector3.zero;
+    void Update()
+    {
 
-    ////        SpawnPoint = GameObject.FindObjectOfType<SpawnPoint>();
-    ////        SpawnPoint.Spawn();
-    ////    } 
-    ////}
+        if (Ball.position.y < yOffset )
+        {
+            Rigidbody BallRB = Ball.GetComponent<Rigidbody>();
+
+            //zet angularVelocity en velocity to 0
+            BallRB.angularVelocity = Vector3.zero;
+            BallRB.velocity = Vector3.zero;
+
+            SpawnPoint = GameObject.FindObjectOfType<SpawnPoint>();
+            SpawnPoint.Spawn();
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
