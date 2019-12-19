b68b393fc2a7038e2466472e3af12171467d1b65
GregoirBrent <brentgregoir@hotmail.com>
2019-12-19 23:14:07 +0100

    last changes

--- MobileVSRealityAR/Assets/Scripts/GameScripts/ScoringCollider.cs
+++ MobileVSRealityAR/Assets/Scripts/GameScripts/ScoringCollider.cs
@@ -19,11 +19,16 @@ public class ScoringCollider : MonoBehaviour
     {
         Debug.Log("BALL IN HOLE! NICE DONE!!!");
 
-        ScoreWindow.ActiveScoreboard();
-        UIElements.SetActive(false);
 
-        Ball = GameObject.FindObjectOfType<Ball>();
-        Ball.gameObject.layer = LayerMask.NameToLayer("Default");
+        if (other.gameObject.tag == "Player")
+        {
+            ScoreWindow.ActiveScoreboard();
+            UIElements.SetActive(false);
+
+            Ball = GameObject.FindObjectOfType<Ball>();
+            Ball.gameObject.layer = LayerMask.NameToLayer("Default");
+
+        }
 
         //Rigidbody BallRB = Ball.GetComponent<Rigidbody>();
         //BallRB.angularVelocity = Vector3.zero;
